922ef36537c84c5f64a8c11bcec966f5232cca01
HenriqueSagawaIFPR <20233017485@estudantes.ifpr.edu.br>
2025-09-19 10:47:51 -0300

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link"
+
 export function Footer() {
   return (
     <footer className="bg-muted py-8 mt-16">
       <div className="container mx-auto px-4 text-center">
-        <div className="flex items-center justify-center gap-2 text-muted-foreground mb-2">
+        <div className="flex items-center justify-center flex-col gap-2 text-muted-foreground mb-2">
           <span>Desenvolvido com</span>
           ☕
-          <span>pelos estudantes do IFPR</span>
+          <span>pelos estudantes do IFPR e mantido pelos servidores</span>
+          <Link href="https://github.com/HenriqueSagawaIFPR/vitrine-projetos-ifpr" target="_blank"></Link>
         </div>
         <p className="text-sm text-muted-foreground">
           © {new Date().getFullYear()} Instituto Federal do Paraná - Todos os direitos reservados
@@ -14,4 +17,5 @@ export function Footer() {
     </footer>
   )
 }
+
 
